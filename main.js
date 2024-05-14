5e7e0de9a265a269da746678a2ba2407997017e8
DUYBEGINER <phucduygg8485@gmail.com>
2024-05-14 22:21:23 +0700

    comback

---
+++ main.js
@@ -0,0 +1,26 @@
+var bttvar1 = document.getElementById('btt1');
+
+function Toggle1(){
+    if(bttvar1.style.color == "red"){
+        bttvar1.style.color="grey"
+    }else{
+        bttvar1.style.color = "red"
+    }
+}
+
+var bttvar2 = document.getElementById('btt2');
+
+function Toggle2(){
+    if(bttvar2.style.color == "red"){
+        bttvar2.style.color="grey"
+    }else{
+        bttvar2.style.color = "red"
+    }
+}
+
+var lg = document.getElementById('swbr');
+
+
+function Switchs(){
+    b
+}
